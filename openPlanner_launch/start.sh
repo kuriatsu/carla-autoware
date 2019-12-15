8d324e4bf34f28ac5349f5d8b1ab4ea0267753c4
kuriatsu <kuriatsubayashi712@gmail.com>
2019-12-15 11:33:53 +0000

    first commit

---
+++ openPlanner_launch/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+#there is a bug in Autoware, that prevents the stack from starting with very low timestamps
+#https://github.com/autowarefoundation/autoware/issues/2200
+#
+#As a workaround we wait 5 seconds in simulation time
+echo "Waiting for 5 seconds in simulation time..."
+python << EOF
+import sys
+import rospy
+from rosgraph_msgs.msg import Clock
+
+def on_clock(data):
+    if data.clock.to_sec() >= 5:
+        rospy.signal_shutdown("")
+
+rospy.init_node("sleep", anonymous=True)
+subscriber = rospy.Subscriber("/clock", Clock, on_clock)
+rospy.spin()
+EOF
+echo "Starting Autoware."
+
+roslaunch $CARLA_AUTOWARE_ROOT/challenge.launch
+
+exit $?
